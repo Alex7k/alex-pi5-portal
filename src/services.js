ac2ac28112ab2b8c7a4053902e92cf09dccfc72b
Alex7k <51133872+Alex7k@users.noreply.github.com>
2025-12-03 09:54:03 +0000

    Init

---
+++ src/services.js
@@ -0,0 +1,29 @@
+const host = "alex-pi5";
+
+const media = {
+  Jellyfin: 8096,
+  Jellyseer: 5055,
+  Sonarr: 8989,
+  Radarr: 7878,
+  Prowlarr: 9696,
+  qBitTorrent: 8080,
+};
+
+const sensors = {
+  Exporter: "9100/metrics",
+  Prometheus: 9090,
+  Grafana: 3000,
+};
+
+function populate(id, items) {
+  const ul = document.getElementById(id);
+  for (const [name, port] of Object.entries(items)) {
+    const url = `http://${host}:${port}`;
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="${url}">${name}</a>`;
+    ul.appendChild(li);
+  }
+}
+
+populate("media", media);
+populate("sensors", sensors);
